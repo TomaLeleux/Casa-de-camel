18ccb7f5629fd971df2ce4a5d058d4e8081ca903
MartinLemiere <mlemiere@norauto.fr>
2018-05-28 17:27:23 +0200

    Seed/PrepareCamelSeed_and_AddGemFaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+CAMELNAME = ['Bob','Bab','Bib','Ouali','Wall-e','Case','Leon','John','Albert','Jean','John','Fred','Seb','Claude']
+
+# 10.times do
+# camel = Camel.create!(
+# name: CAMELNAME.sample,
+# localisation: Faker::Address.full_address,
+# number_of_place: [1,2].sample,
+# food_type: ['Grass','Fruit','Vegetables','Carn'].sample,
+# color: ['Red','Blue','Camel','Orange','White','Brown'],
+# user: User.first,
+# wifi: [true,false].sample,
+# chameaux_fiscaux: [4,5,6,7].sample,
+# vitesse_max: [5,10,15,20,25,30,50,100].sample
+# )
+# end
