0d1fd180fc02f41c0e9888832736d888b9bb7d28
Maxime Favret <favretmaxime@gmail.com>
2018-05-28 18:16:30 +0200

    updated routes but not yet finished #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/camels', to: 'camels#index'
+  get '/camels/new', to: 'camels#new'
+  post '/camels', to: 'camels#create'
+  get '/camels/:id', to: 'camels#show'
+  get '/camels/:id/edit', to: 'camels#edit'
+  get '/camels/:id', to: 'camels#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
