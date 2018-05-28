964da52b87182994f1ed5001627eb6a4460de00a
MartinLemiere <mlemiere@norauto.fr>
2018-05-28 17:55:02 +0200

    Seed ok for 2 user and 10 camel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,25 @@
 CAMELNAME = %w(Bob Bab Bib Ouali Walle Case Leon John Albert Jean John Fred Seb Claude)
+user = User.new(email: 'toto@camel.com')
+user.password = '123456'
+user.password_confirmation = '123456'
+user.save
 
-# 10.times do
-# camel = Camel.create!(
-# name: CAMELNAME.sample,
-# localisation: Faker::Address.full_address,
-# number_of_place: [1,2].sample,
-# food_type: ['Grass','Fruit','Vegetables','Carn'].sample,
-# color: ['Red','Blue','Camel','Orange','White','Brown'],
-# user: User.first,
-# wifi: [true,false].sample,
-# chameaux_fiscaux: [4,5,6,7].sample,
-# vitesse_max: [5,10,15,20,25,30,50,100].sample
-# )
-# end
+user2 = User.new(email: 'tata@camel.com')
+user2.password = '123456'
+user2.password_confirmation = '123456'
+user2.save
+
+
+10.times do
+camel = Camel.create!(
+name: CAMELNAME.sample,
+localisation: Faker::Address.full_address,
+number_of_place: [1,2].sample,
+food_type: ['Grass','Fruit','Vegetables','Carn'].sample,
+color: ['Red','Blue','Camel','Orange','White','Brown'].sample,
+user: [User.first,User.last].sample,
+wifi: [true,false].sample,
+chameaux_fiscaux: [4,5,6,7].sample,
+vitesse_max: [5,10,15,20,25,30,50,100].sample
+)
+end
