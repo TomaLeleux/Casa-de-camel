e37b1f1e79da60b672c6ccb205e3b8688858b3db
MartinLemiere <mlemiere@norauto.fr>
2018-05-29 11:31:35 +0200

    Add photo:string to User, update navbar with user:photo if logged and existent, add unlogged and default picture #13

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_161749) do
+ActiveRecord::Schema.define(version: 2018_05_29_085615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_161749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.integer "price_per_day"
     t.index ["user_id"], name: "index_camels_on_user_id"
   end
 
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_161749) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
