534977ed7e447a57137983c988578f0e8ec91ebf
Thomas Leleux <leleux.thomas@gmail.com>
2018-05-29 16:34:07 +0200

    Edit links in navbar

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,15 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def show?
+    return true
+  end
+
+  def dashboard?
+    return true
+  end
+end
