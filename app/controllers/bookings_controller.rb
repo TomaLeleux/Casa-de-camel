721c7e91bf755385d9d26691d87fc7fc804cf167
Thomas Leleux <leleux.thomas@gmail.com>
2018-05-30 17:22:22 +0200

    correct redirect

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,7 +23,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to bookings_path
     else
-      render "bookings#show"
+      render camel_path(@camel)
     end
   end
 
